bebb5c28ac707d831ca5ea3fe7fce37f4896c113
torahoo <legokbs@gmail.com>
2023-04-18 19:22:50 +0900

    feature/#7-회원 도메인 개발 : 1. 회원 서비스 개발 2. @RequiredArgsConstructor 추가 및 수정

--- src/main/java/jpbook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpbook/jpashop/repository/MemberRepository.java
@@ -1,17 +1,17 @@
 package jpbook.jpashop.repository;
 
 import jpbook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
 
-    @PersistenceContext
-    private EntityManager em;
+    private final EntityManager em; // persistance 어노테이션이 있어야지 Entity가 작동을 하지만 스프링 부트는 자동 지원
 
     public void save (Member member) {
         em.persist(member);
---
+++ src/main/java/jpbook/jpashop/service/MemberService.java
@@ -0,0 +1,58 @@
+package jpbook.jpashop.service;
+
+import jpbook.jpashop.domain.Member;
+import jpbook.jpashop.repository.MemberRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor // final이 걸린 필드만 생성자로 엮어 만들어줌
+public class MemberService {
+
+    private final MemberRepository memberRepository;
+
+//    public MemberService(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    } // 최근 스프링은 @AutoWired 어노테이션이 없어도 자동으로 인젠션 해줌
+
+    /**
+     * 회원 가입
+     */
+    @Transactional
+    public Long join (Member member) {
+        validateDuplicateMember(member); // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    /**
+     * 중복 회원 검증
+     */
+    private void validateDuplicateMember(Member member) {
+        //exeption
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        }
+    }
+
+    /**
+     * 회원 전체 조회
+     */
+    public List<Member> findMembers () {
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 회원 하나 조회
+     */
+    public Member findOne (Long memberId) {
+        return memberRepository.findOne(memberId);
+    }
+}
