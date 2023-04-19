2b179a04e064e492cf6b961fbd6d7d0c9593c20a
torahoo <legokbs@gmail.com>
2023-04-19 20:50:30 +0900

    feature/#11 - 주문 도메인 개발 : 1. 주문 서비스 개발

--- src/main/java/jpbook/jpashop/domain/OrderItem.java
+++ src/main/java/jpbook/jpashop/domain/OrderItem.java
@@ -1,7 +1,9 @@
 package jpbook.jpashop.domain;
 
 import jpbook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -11,6 +13,7 @@ import static javax.persistence.FetchType.LAZY;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
 
     @Id @GeneratedValue
@@ -27,6 +30,8 @@ public class OrderItem {
 
     private int orderPrice; //주문 가격
     private int count; // 주문 수량
+
+//    protected OrderItem (){} //기본생성자를 막아 set 막기
 
     //==생성 메서드==//
     public static OrderItem createOrderItem (Item item, int orderPrice, int count) {
